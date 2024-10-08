994816e898eb8cd2ad7dd2303c805dfffc72a191
Aaron Ky-Riesenbach <aaron.ky-riesenbach@wolterskluwer.com>
2024-10-08 09:55:12 -0400

    Update Cherry Hill URL

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from models.Library import Library
 
 libraries = [
     Library("BPL", "https://bpl.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&"),
-    Library("Cherry Hill", "https://sjrlc.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&"),
+    Library("Cherry Hill", "https://chplnj.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&"),
     Library("Northeastern", "https://northeasternuni.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&"),
     Library("LAPL", "https://lapl.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&"),
     Library("South Pas", "https://scdl.overdrive.com/search?showOnlyAvailable=true&format=ebook-kindle&")
